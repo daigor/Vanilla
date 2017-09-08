587f1763cbd2c237d9c33c113b997aedb306ab42
Diego Alejandro Rodriguez Ayala <diego.magno120@gmail.com>
2017-09-08 16:13:55 -0500

    Updated bash-script (C++.sh) to read input arguments with spaces and created folder for it

--- script/C++.sh
+++ script/C++.sh
@@ -5,16 +5,16 @@
 #Give permission to execute $chmod u+x C++.sh
 #Example $./C++ HelloWorld.cpp
 
-if [ -f $1 ]; then
+if [ -f "$1" ]; then
   echo "Make & Run - $1"
   compiledFile=${1%.cpp}.out
-  g++ $1 -o $compiledFile
+  g++ "$1" -o "$compiledFile"
   if [ $? -ne 0 ]; then
     echo "Error compiling code: $1"
   else
       echo -e "Executing...\n"
-      ./$compiledFile
+      ./"$compiledFile"
   fi
 else
-  echo "File $1 does not exist"
+  echo -e "\nFile $1 does not exist"
 fi
---
+++ script/hello world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout<<"Hello, hell of world... with spaces included! (• ε •)"<<endl;
+  return 0;
+}
